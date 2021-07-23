84ed8fe9913724c31243abc2f8d86e56c861e4dc
vchimania <vinichimania98@gmail.com>
2021-07-23 12:43:10 +0530

    CheckOut

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React from 'react';
 
-const Header=(props)=>{
-
-    return(
-        <header className="block row center">
+export default function Header(props) {
+  return (
+    <header className="block row center">
       <div>
         <a href="#/">
           <h1>Shopping Cart</h1>
         </a>
       </div>
       <div>
-        <a href="#/cart">Cart</a>
+        <a href="#/cart">
+          Cart{' '}
+          {props.countCartItems ? (
+            <button className="badge">{props.countCartItems}</button>
+          ) : (
+            ''
+          )}
+        </a>{' '}
         <a href="#/signin"> SignIn</a>
       </div>
     </header>
-    )
+  );
 }
-
-export default Header
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,16 @@
-import React from "react";
+import React from 'react';
 import Product from './Product';
 
-const Main=(props)=>{
- const{products}=props;
-    return(
-        <main className="block col-2">
-        <h2>Products</h2>
-        {/* <div className="container"> */}
-        <div className="row  ">
-        <div class="col-md-4">
+export default function Main(props) {
+  const { products, onAdd } = props;
+  return (
+    <main className="block col-2">
+      <h2>Products</h2>
+      <div className="row">
         {products.map((product) => (
-          <Product key={product.id} product={product}></Product>
+          <Product key={product.id} product={product} onAdd={onAdd}></Product>
         ))}
-            </div>
-            </div>
-            {/* </div> */}
-            </main>
-    )
+      </div>
+    </main>
+  );
 }
-
-export default Main
